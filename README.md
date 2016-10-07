5af0bbdebd8480984853ca728a5ccf8ceedde481
Jann Mueller <j.mueller.11@ucl.ac.uk>
2016-10-07 10:10:37 +0100

    Add example form

---
+++ README.md
@@ -0,0 +1,20 @@
+# virtual-hom-example
+
+Virtual hom components example
+
+# How to build
+
+```
+stack setup
+stack install
+```
+
+than open index.html.
+
+# License
+
+BSD3 (see LICENSE file)
+
+## Contributions
+
+Bug reports, pull requests, feature requests are welcome
---
+++ src/Main.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import           Control.Applicative
+import           Control.Lens hiding (children)
+import           Data.Monoid
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           VirtualHom.Components
+import           VirtualHom.Element(attributes, callbacks, children, content, value)
+import qualified VirtualHom.Element as E
+import qualified VirtualHom.Html as H
+import           VirtualHom.Rendering(renderingOptions)
+
+newtype FirstName = FirstName { getFirstName :: Text }
+newtype LastName = LastName { getLastName :: Text }
+
+validFirstName :: Text -> Either Text FirstName
+validFirstName t = if T.length t > 0 then Right (FirstName t) else Left "First name must not be empty" 
+
+validLastName :: Text -> Either Text LastName
+validLastName t = if T.length t > 0 then Right (LastName t) else Left "Last name must not be empty"
+
+data Person = Person {
+  _firstName :: FirstName,
+  _lastName  :: LastName
+}
+makeLenses ''Person
+
+
+validatingTextInput :: Monad m => (Text -> Either Text a) -> Component m (Maybe a)
+validatingTextInput f = component "" $ return . result where
+  errorMarker = either (const "has-error") (const "") . f
+  validated = either (const Nothing) Just . f
+  result (textValue, a) = H.div
+    & attributes . at "class" ?~ ("form-group " <> errorMarker textValue)
+    & children .~ [
+        H.input
+          & attributes . at "class" ?~ "form-control"
+          & attributes . at "type"  ?~ "text"
+          & attributes . at "value" ?~ textValue
+          & callbacks . E.change ?~ (\e _ -> return (e^.value, validated $ e^.value))
+    ]
+
+data PersonForm m = PersonForm {
+  _fn :: Maybe FirstName,
+  _ln :: Maybe LastName,
+  _firstNameInput :: Component m (Maybe FirstName),
+  _lastNameInput  :: Component m (Maybe LastName) 
+}
+makeLenses ''PersonForm
+
+personForm :: Monad m => PersonForm m
+personForm = PersonForm Nothing Nothing i1 i2 where
+  i1 = validatingTextInput validFirstName
+  i2 = validatingTextInput validLastName
+
+personComp :: Monad m => Component m ()
+personComp = component personForm $ \tpl -> [ container & children .~ [
+  row & children .~ [H.h1 "Edit contact"],
+  row & children .~ subComponent (state.fn) (state.firstNameInput) tpl,
+  row & children .~ subComponent (state.ln) (state.lastNameInput)  tpl
+  ] <> (maybe [] personLabel $ tpl^.state.to person)]
+
+personLabel :: Person -> [E.Elem cb ()]
+personLabel p = [row & children .~ [
+  H.div 
+    & attributes . at "class" ?~ "alert alert-success"
+    & content .~ "Hello, " <> personText p
+  ]]
+
+main :: IO ()
+main = do
+  let options = renderingOptions "virtual-hom"
+  let interp = return . runIdentity
+  renderComponent options personComp interp ()
+
+-- | Utilities
+
+personText :: Person -> Text
+personText p = p^.firstName.to getFirstName <> " " <> p^.lastName.to getLastName
+
+person :: PersonForm m -> Maybe Person
+person pf = Person <$> view fn pf <*> view ln pf 
+
+-- | Bootstrap CSS
+container = H.div & attributes . at "class" ?~ "container"
+row = H.div & attributes . at "class" ?~ "row"
